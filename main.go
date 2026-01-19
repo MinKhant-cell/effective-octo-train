066b8cde6d8bd01483870fa032d98529dd87d14c
MinKhant-cell <mink75311@gmail.com>
2026-01-19 13:20:57 +0700

    init

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+
+	"product-api/database"
+	"product-api/models"
+)
+
+func main() {
+	// Connect to database
+	database.Connect()
+
+	// Auto migrate
+	database.DB.AutoMigrate(&models.Product{})
+
+	r := gin.Default()
+
+	// Root route
+	r.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Hello, welcome to my store!")
+	})
+
+	// Products route
+	r.GET("/products", func(c *gin.Context) {
+		var products []models.Product
+		database.DB.Find(&products)
+		c.JSON(http.StatusOK, products)
+	})
+
+	r.Run(":8080")
+}
